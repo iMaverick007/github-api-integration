91a97638d60476807e475ad0a7457c0e5b7f46e8
iMaverick007 <alloydfernandes007@gmail.com>
2025-03-09 12:24:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const githubToken = process.env.GITHUB_TOKEN;
